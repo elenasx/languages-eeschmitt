37187b2b818996d140623b453af2c29f4e679682
elenasx <eeschmitt@mavs.coloradomesa.edu>
2024-12-05 21:37:40 -0700

    fixed regex

--- regex/ruby/spec/num_spec.rb
+++ regex/ruby/spec/num_spec.rb
@@ -6,12 +6,16 @@ RSpec.describe 'Regular Expression for decimal literal' do
   let(:hex_digit) {  /[0-9]|[A-F]|[a-f]/ }
   let(:binary_digit) {  /[0-1]/ }
   let(:decimal) { /#{nonzero_decimal_digit}('?#{decimal_digit}+)*/ }
+  # Add octal, hex, and binary
+  let(:octal) { /0#{octal_digit}+/ } # base 8
+  let(:hex) { /0[xX]#{hex_digit}+/ } # base 16, letters a through f
+  let(:binary) { /0[bB]#{binary_digit}+/ } # base 2
   let(:size) { /[uU]?[lL]{0,2}/ }
 
-  let(:pattern) { /^-?#{decimal}#{size}$/ }
+  let(:pattern) { /^-?(#{decimal}|#{octal}|#{hex}|#{binary})#{size}$/ }
 
-  let(:should_pass) { [ "1", "-33'000", "4525235", "10'080", "123'456'789", "1ul", "1u", "1ll" ] }
-  let(:should_fail) { ["'1'", "1'''3", "afed", "+33", "0", "ul", "lll", "3lll", "3uuull" ] }
+  let(:should_pass) { [ "1", "-33'000", "4525235", "10'080", "123'456'789", "1ul", "1u", "1ll", "0b10011", "0x5f", "023", "9'999" ] }
+  let(:should_fail) { ["'1'", "1'''3", "afed", "+33", "0", "ul", "lll", "3lll", "3uuull", "0b832", "0xH1", "088", "3'''234" ] }
 
   describe 'should pass' do
     it 'matches all expected strings' do
